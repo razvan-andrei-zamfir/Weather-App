a1d07f6cdd46ea43831cbd2a5bc491773954b5ed
razvan-andrei-zamfir <razvan.zamfir.986@gmail.com>
2024-09-13 19:59:14 +0200

    Added dynamic background image

--- main.js
+++ main.js
@@ -45,8 +45,7 @@ let weather = {
     document.querySelector(".wind").innerText =
       "Wind speed: " + roundedSpeed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1920x1080/?" + name + "')";
+    document.body.style.backgroundImage = "url('https://picsum.photos/1920/1080')";
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
